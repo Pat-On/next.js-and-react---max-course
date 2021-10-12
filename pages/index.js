6e842220aee46baa3b79cdd56a660be004a20d4c
Patryk <patryk.r.nowak@gmail.com>
2021-10-12 17:18:28 +0100

    v58

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+// it is going to be home root
+
+function HomePage() {
+  return (
+    <div>
+      <h1>The Home Page</h1>
+    </div>
+  );
+}
+
+export default HomePage;
---
+++ pages/portfolio/about/index.js
@@ -0,0 +1,10 @@
+function AboutPage() {
+  return (
+    <div>
+      {" "}
+      <h1>The About Page 2 inside the folder</h1>
+    </div>
+  );
+}
+
+export default AboutPage;
